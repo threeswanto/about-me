984bd7a965f42977466d5c2efbb326b9a523e2c7
tris_yoko <61571887+threeswanto@users.noreply.github.com>
2021-08-24 14:54:42 +0700

    Initial Project

---
+++ app/src/main/java/com/triswanto/aboutme/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.triswanto.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
